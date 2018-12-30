260456315af0083f8eed02fb86135784fcbc1f10
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-12-30 16:42:20 +0100

    cleanup

--- app/pages/home/HomePage.vue
+++ app/pages/home/HomePage.vue
@@ -60,8 +60,7 @@
     },
     data() {
       return {
-        tab1Msg: "Team pagina hier? 🎳",
-        tabIndex: 1, // setting this initially makes dev a little easier
+        tabIndex: 0, // setting this initially makes dev a little easier
         tabView: undefined, // set below
         isOfficial: true,
         logoSrc: "~/assets/images/fpr-logo-128.png"
