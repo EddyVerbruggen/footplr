09f1d31a91e5d348235e0adea583544ca43e54fb
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-01-07 18:47:51 +0100

    Added situps

--- app/pages/home/components/measurements/measurement-entry/AddMeasurementForExercise.vue
+++ app/pages/home/components/measurements/measurement-entry/AddMeasurementForExercise.vue
@@ -3,6 +3,7 @@
     <Dribble :player="player" v-if="exercise === 'DRIBBLE'"></Dribble>
     <Heartrate :player="player" v-if="exercise === 'HEARTRATE'"></Heartrate>
     <PushUps :player="player" v-if="exercise === 'PUSH_UPS'"></PushUps>
+    <SitUps :player="player" v-if="exercise === 'SIT_UPS'"></SitUps>
   </StackLayout>
 </template>
 
@@ -10,12 +11,14 @@
   import Dribble from "./exercise/Dribble";
   import Heartrate from "./exercise/Heartrate";
   import PushUps from "./exercise/PushUps";
+  import SitUps from "./exercise/SitUps";
 
   export default {
     components: {
       Dribble,
       Heartrate,
       PushUps,
+      SitUps,
     },
 
     props: ['exercise', 'player'],
