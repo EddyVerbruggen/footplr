2b6e75cc49e2973129a43a1b1c0b693e62c373aa
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-01-07 17:33:21 +0100

    This will not work, because the timer is outside the component.. better to fix with a clear instruction in this case

--- app/pages/home/components/measurements/measurement-entry/exercise/Heartrate.vue
+++ app/pages/home/components/measurements/measurement-entry/exercise/Heartrate.vue
@@ -1,7 +1,14 @@
 <template>
   <GridLayout columns="*, 10, *" horizontalAlignment="center">
-    <NumericKeyboard col="0" ref="nrOfBeats1" @blur="blurNrOfBeats" hint="Meting 1" locale="nl_NL" noDecimals="true" returnKeyTitle="OK" horizontalAlignment="center" class="numeric-input"></NumericKeyboard>
-    <NumericKeyboard col="2" ref="nrOfBeats2" @blur="blurNrOfBeats" hint="Meting 2" locale="nl_NL" noDecimals="true" returnKeyTitle="OK" horizontalAlignment="center" class="numeric-input"></NumericKeyboard>
+    <!--SegmentedBar row="0" colSpan="3" class="m-t-20" @selectedIndexChange="onSelectedIndexChanged" horizontalAlignment="center">
+      <SegmentedBarItem title="zonder hartslagmeter"></SegmentedBarItem>
+      <SegmentedBarItem title="gebruik hartslagmeter"></SegmentedBarItem>
+    </SegmentedBar-->
+
+    <NumericKeyboard col="0" ref="nrOfBeats1" @blur="blurNrOfBeats" hint="Meting 1" locale="nl_NL" noDecimals="true"
+                     returnKeyTitle="OK" horizontalAlignment="center" class="numeric-input"></NumericKeyboard>
+    <NumericKeyboard col="2" ref="nrOfBeats2" @blur="blurNrOfBeats" hint="Meting 2" locale="nl_NL" noDecimals="true"
+                     returnKeyTitle="OK" horizontalAlignment="center" class="numeric-input"></NumericKeyboard>
   </GridLayout>
 </template>
 
@@ -12,10 +19,21 @@
   export default {
     props: ['player'],
 
+    data() {
+      return {
+        hasHeartrateDevice: false,
+      }
+    },
+
     methods: {
+      onSelectedIndexChanged() {
+        this.hasHeartrateDevice = !this.hasHeartrateDevice;
+      },
+
       blurNrOfBeats() {
         if (this.$refs.nrOfBeats1.nativeView.text && this.$refs.nrOfBeats2.nativeView.text) {
-          const measurement = (this.$refs.nrOfBeats1.nativeView.text * 4) - (this.$refs.nrOfBeats2.nativeView.text * 4);
+          const factor = this.hasHeartrateDevice ? 1 : 4;
+          const measurement = (this.$refs.nrOfBeats1.nativeView.text * factor) - (this.$refs.nrOfBeats2.nativeView.text * factor);
           EventBus.$emit("score-entered", {measurement, player: this.player});
         }
       }
