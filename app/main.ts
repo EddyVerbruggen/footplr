eea23c1a63aae356b7793a78cd34b99e88da2eb5
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-01-10 15:07:59 +0100

    Added Crashlytics

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,13 @@
+import * as firebase from "nativescript-plugin-firebase"
 import Vue from "nativescript-vue"
+import * as application from "tns-core-modules/application";
 import { isIOS } from "tns-core-modules/platform";
 import ApplicationSettingsService from "~/services/ApplicationSettingsService";
+import { EventBus } from "~/services/event-bus";
 import { onAndroidKeyboardShowingListener } from "~/utils/keyboard-util";
 import routes from "./router";
 import AuthService from "./services/AuthService"
 import EditingUserService from "./services/EditingUserService"
-import * as firebase from "nativescript-plugin-firebase"
-import {EventBus} from "~/services/event-bus";
-
 // To include the stylesheets, use this:
 import "./styles.scss";
 
@@ -42,20 +42,35 @@ setTimeout(() => {
 }, 2000);
 
 firebase.init()
-  .then(instance => {
-    /*
-    firebase.registerForPushNotifications(
-        {
-          showNotifications: true,
-          showNotificationsWhenInForeground: true,
-          onPushTokenReceivedCallback: token => console.log(`------------------- token received: ${token}`),
-          onMessageReceivedCallback: message => console.log(`------------------- message received`)
-        })
-        .then(instance => console.log("registerForPushNotifications done"))
-        .catch(error => console.log(`-------------- registerForPushNotifications error: ${error}`));
-    */
-  })
-  .catch(error => console.log(`firebase.init error: ${error}`));
+    .then(instance => {
+      /*
+      firebase.registerForPushNotifications(
+          {
+            showNotifications: true,
+            showNotificationsWhenInForeground: true,
+            onPushTokenReceivedCallback: token => console.log(`------------------- token received: ${token}`),
+            onMessageReceivedCallback: message => console.log(`------------------- message received`)
+          })
+          .then(instance => console.log("registerForPushNotifications done"))
+          .catch(error => console.log(`-------------- registerForPushNotifications error: ${error}`));
+      */
+
+      application.on(application.uncaughtErrorEvent, args => {
+        // TODO send to crashlytics
+        if (application.android) {
+          // For Android applications, args.android is an NativeScriptError.
+          console.log(" *** NativeScriptError *** : " + args.android);
+          console.log(" *** StackTrace *** : " + args.android.stackTrace);
+          console.log(" *** nativeException *** : " + args.android.nativeException);
+          firebase.crashlytics.sendCrashLog(args.android.nativeException);
+        } else if (application.ios) {
+          // For iOS applications, args.ios is NativeScriptError.
+          console.log(" *** NativeScriptError  *** : " + args.ios);
+          firebase.crashlytics.sendCrashLog(args.ios);
+        }
+      });
+    })
+    .catch(error => console.log(`firebase.init error: ${error}`));
 
 // Prints Vue logs when --env.production is *NOT* set while building
 v.config.silent = true; // (TNS_ENV === 'production');
