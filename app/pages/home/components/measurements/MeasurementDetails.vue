8da66d169a2ce651b63b8a44b384e9d4d3b203fe
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-08-15 18:19:42 +0200

    FPR-5 Optimaliseren van de grafieken (score '100' paste niet)

--- app/pages/home/components/measurements/MeasurementDetails.vue
+++ app/pages/home/components/measurements/MeasurementDetails.vue
@@ -15,7 +15,7 @@
 
       <!--<StackLayout row="2" class="c-bg-white"></StackLayout>-->
 
-      <GridLayout row="3" colSpan="2" columns="50, 76, *, 100" class="table" style="background-color: #011627; color: #fff">
+      <GridLayout row="3" colSpan="2" columns="54, 76, *, 100" class="table" style="background-color: #011627; color: #fff">
         <Label col="0" text="Score" class="m-l-10 p-y-10 bold" horizontalAlignment="center"></Label>
         <Label col="1" text="Meting" class="p-y-10 p-x-10 bold" horizontalAlignment="right"></Label>
         <Label col="2" text="Datum" class="p-y-10 p-x-5 bold"></Label>
@@ -24,7 +24,7 @@
       <ListView row="4" colSpan="2" for="(item, index) in measurements" @itemTap="onItemTap"
                 separatorColor="transparent" class="table c-bg-white">
         <v-template>
-          <GridLayout columns="50, 76, *, 100" class="row" v-bind:class="index % 2 === 0 ? 'row-odd' : 'row-even'">
+          <GridLayout columns="54, 76, *, 100" class="row" v-bind:class="index % 2 === 0 ? 'row-odd' : 'row-even'">
             <Label col="0" :text="item.score" :class="'background-color-score-' + item.scoreClass"
                    class="m-l-10 m-y-4 p-y-5 p-x-5 score bold" horizontalAlignment="center"></Label>
             <Label col="1" color="#011627" :text="item.measurement" class="p-y-10 p-x-10"
@@ -191,7 +191,7 @@
 
   .table .score {
     color: #fff;
-    width: 30;
+    width: 34;
     text-align: center;
     border-radius: 3;
   }
